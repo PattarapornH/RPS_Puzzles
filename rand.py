ebb13c80d5e12628b707fbfb5f9a35c1ccf1307f
PattarapornH <pataraporn.han@gmail.com>
2017-10-03 01:11:38 +0700

    random rps

---
+++ rand.py
@@ -0,0 +1,21 @@
+
+from random import randint
+x = randint(0,600)
+
+#class rand_rps:
+##while(time>0):
+#def red:
+if(x<100):
+    print("red rock")       #return 1
+elif (x>=100 and x<200):    
+    print("red paper")      #return 2
+elif (x>=200 and x<300): 
+    print("red scrisor")    #return 3
+
+#def blue:
+if(x>=300 and x<400):
+    print("blue rock")      #return 4
+elif (x>=400 and x<500):
+    print("blue paper")     #return 5
+elif (x>=500 and x<600):
+    print("blue scrissor")  #return 6
