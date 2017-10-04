baf95a87e3129020f45138a7b14a2183471e9ce2
PattarapornH <pataraporn.han@gmail.com>
2017-10-04 23:39:22 +0700

    set parameter

--- timer.py
+++ timer.py
@@ -2,17 +2,11 @@
 import arcade
 
 class time():
-    def __init__(self):
-        """
-        Set up the application.
-        """
-        self.total_time = 60.0
+    def __init__(self,time):
+        self.total_time = time
         self.timer_text = None
         self.minutes = int(self.total_time) // 60
         self.seconds = int(self.total_time) % 60
 
     def update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        """
         self.total_time -= delta_time
