f304a1e830d8936a6b7eba4b7a9b4ef1d6456aa5
PattarapornH <pataraporn.han@gmail.com>
2017-10-04 17:48:55 +0700

    set random number

--- rand.py
+++ rand.py
@@ -1,18 +1,18 @@
 
 from random import randint
-x = randint(1,6)
-print(x)
 
-if(x == 1):
-    print("red rock")       #return 1
-elif (x == 2):    
-    print("red paper")      #return 2
-elif (x == 3): 
-    print("red scrisor")    #return 3
 
-if(x == 4):
-    print("blue rock")      #return 4
-elif (x == 5):
-    print("blue paper")     #return 5
-elif (x == 6):
-    print("blue scrissor")  #return 6
+class rand_rps:
+    def __init__(self):
+        self.x = randint(1,7)
+        self.y = randint(8,12)
+
+    '''
+    x = 1               red rock 
+    x = 2               red paper
+    x = 3               red scrisor      
+    x = 4               blue rock
+    x = 5               blue paper
+    x = 6               blue scrissor
+    x = 7 and y=8       add time
+    '''
