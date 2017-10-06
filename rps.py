75391df9d10cbb5865442bf1eb61229a7a7cb031
PattarapornH <pataraporn.han@gmail.com>
2017-10-06 11:41:23 +0700

    incomplete render sign & set op key press & can't get score

--- rps.py
+++ rps.py
@@ -3,7 +3,7 @@ import arcade
 from rand import rand_rps
 from red import Red_against
 from blue import Blue_against
-from timer import time
+#from timer import time
 from scorer import score
 
 SCREEN_WIDTH = 400
@@ -24,33 +24,96 @@ class gameWindow(arcade.Window):
         super().__init__(width,height)
         self.background = arcade.load_texture("images/bg1.jpg")
         self.rand = rand_rps()
-        self.x = self.rand.x
-        self.y = self.rand.y
-        self.sc = score()
+    #    self.x = 1
+    #    self.y = self.rand.y
+        self.a = Red_against()
+    #    self.sc = score()
+        self.score = 0
         self.score_text = None
-        print(self.x)
-  #      self.x = rand_rps.x
-   #     self.y = rand.rand_rps.y
+     #   print(self.x)
     
     def on_draw(self):
         arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
-        output = f"Score : {self.sc.score}"
+        output = f"Score : {self.score}"
         if not self.score_text or self.score_text.text != output:
             self.score_text = arcade.create_text(output, arcade.color.BLACK, 30)
         arcade.render_text(self.score_text,100,100)
-        if(self.x>=1):
+        ##RED
+        if(self.x == 1):
             self.rock = arcade.Sprite('images/fist  red.png')
             self.rock.set_position(200,300)
             self.rock.draw()
+        elif(self.x == 2):
+            self.paper = arcade.Sprite('images/hand red.png')
+            self.paper.set_position(200,300)
+            self.paper.draw()
+        elif(self.x == 3):
+            self.scissor = arcade.Sprite('images/victory-sign (1).png')
+            self.scissor.set_position(200,300)
+            self.scissor.draw()
+        ##BLUE
+        elif(self.x == 4):
+            self.rock = arcade.Sprite('images/fist blue.png')
+            self.rock.set_position(200,300)
+            self.rock.draw()
+        elif(self.x == 5):
+            self.paper = arcade.Sprite('images/hand blue.png')
+            self.paper.set_position(200,300)
+            self.paper.draw()
+        elif(self.x == 6):
+            self.scissor = arcade.Sprite('images/victory-sign.png')
+            self.scissor.set_position(200,300)
+            self.scissor.draw()
+
+    def on_key_press(self,key,key_modifier):
+        Red_against().on_key_press(key,key_modifier)
+
+    def update(self,delta):
+        self.x = rand_rps().x
+        self.y = rand_rps().y
+        ##RED
+        if(self.x==1):
             self.red = Red_against().Rock()
+            if(self.red):
+                self.score+=10
+            else:
+                self.score-=5
+        elif(self.x==2):
+            self.red = Red_against().Paper()
+            if(self.red):
+                self.score+=10
+            else:
+                self.score-=5
+        elif(self.x==3):
+            self.red = Red_against().Scissor()
+            if(self.red):
+                self.score+=10
+            else:
+                self.score-=5
 
- #   def update(self)
-
-
- #   def update(self,delta):
-  #      if(self.rand>=1 and self.rand<=3):
-
+        ##BLUE
+        elif(self.x==4):
+            self.blue = Blue_against().Rock()
+            if(self.blue):
+                self.score+=10
+            else:
+                self.score-=5
+        elif(self.x==5):
+            self.blue = Blue_against().Paper()
+            if(self.blue):
+                self.score+=10
+            else:
+                self.score-=5       
+        elif(self.x==6):
+            self.blue = Blue_against().Scissor()
+            if(self.blue):
+                self.score+=10
+            else:
+                self.score-=5
+        ##set score
+        if(self.score<0):
+            self.score = 0
 
 if __name__ == '__main__':
     window = gameWindow(SCREEN_WIDTH,SCREEN_HEIGHT)
