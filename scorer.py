9b1eb0cf903456a8766259809791a8a3a0b6d52d
PattarapornH <pataraporn.han@gmail.com>
2017-10-06 13:51:17 +0700

    debug colon

--- scorer.py
+++ scorer.py
@@ -1,6 +1,10 @@
 
 class score:
-    def __init__(self)
+    def __init__(self):
         self.score = 0
+    ''' 
+    def update(self,delta):
         if(self.score < 0):
             self.score = 0
+      #  print(self.score)
+    '''
