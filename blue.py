ec294a75e4c5fc927f37806e8ea132bf0db3c00e
PattarapornH <pataraporn.han@gmail.com>
2017-10-15 01:12:51 +0700

    set key press to count score

--- blue.py
+++ blue.py
@@ -4,20 +4,20 @@ A rock
 S paper
 D scissor
 '''
+import arcade
 from rand import rand_rps
-from scorer import score
+#from scorer import score
 
-import arcade.key
 class Blue_against:
     def __init__(self):
-        self.x = rand_rps().x
-        self.y = rand_rps().y
+        self.rand = rand_rps()
+        self.x = self.rand.x
         self.A = False
         self.S = False
         self.D = False
         self.W = False
-
-    def on_key_press(self,key,key_modifiers):
+   
+    def on_key_press(self,key,key_modifier):
         if key == arcade.key.A:
             self.A = True
         elif key == arcade.key.S:
@@ -25,22 +25,25 @@ class Blue_against:
         elif key == arcade.key.D:
             self.D = True
         elif key == arcade.key.W:
-            self.W = True
-
+            self.W = True    
+           
     def Rock(self):
-        if(self.S):
+        if self.S :
+            self.S = False
             return True
         else:
             return False
     
     def Paper(self):
-        if(self.D):
+        if self.D :
+            self.D = False
             return True
         else:
             return False
 
     def Scissor(self):
-        if(self.A):
+        if self.A :
+            self.A = False
             return True
         else:
             return False
