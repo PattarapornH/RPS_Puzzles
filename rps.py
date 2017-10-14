8509959bc212a68b12bc3d8d60eab40f60a6492c
PattarapornH <pataraporn.han@gmail.com>
2017-10-15 01:11:33 +0700

    count score still has a little bug

--- rps.py
+++ rps.py
@@ -1,4 +1,5 @@
 import arcade
+import time
 
 from rand import rand_rps
 from red import Red_against
@@ -24,6 +25,7 @@ class gameWindow(arcade.Window):
         self.rand = rand_rps()
         self.R = Red_against()
         self.B = Blue_against()
+        self.x = self.rand.x
         self.score = 0
         self.score_text = None
      #   print(self.x)
@@ -34,79 +36,84 @@ class gameWindow(arcade.Window):
         output = f"Score : {self.score}"
         if not self.score_text or self.score_text.text != output:
             self.score_text = arcade.create_text(output, arcade.color.BLACK, 30)
-        arcade.render_text(self.score_text,100,100)
-        ##RED
-        if(self.x == 1):
-            self.rock = arcade.Sprite('images/fist  red.png')
-            self.rock.set_position(200,300)
-            self.rock.draw()
-        elif(self.x == 2):
-            self.paper = arcade.Sprite('images/hand red.png')
-            self.paper.set_position(200,300)
-            self.paper.draw()
-        elif(self.x == 3):
-            self.scissor = arcade.Sprite('images/victory-sign (1).png')
-            self.scissor.set_position(200,300)
-            self.scissor.draw()
-        ##BLUE
-        elif(self.x == 4):
-            self.rock = arcade.Sprite('images/fist blue.png')
-            self.rock.set_position(200,300)
-            self.rock.draw()
-        elif(self.x == 5):
-            self.paper = arcade.Sprite('images/hand blue.png')
-            self.paper.set_position(200,300)
-            self.paper.draw()
-        elif(self.x == 6):
-            self.scissor = arcade.Sprite('images/victory-sign.png')
-            self.scissor.set_position(200,300)
-            self.scissor.draw()
+        arcade.render_text(self.score_text,100,400)
+        #### DRAW CHOICE ####
+        self.rock = arcade.Sprite('images/rock.png')
+        self.rock.set_position(100,100)
+        self.rock.draw()
+
+        self.paper = arcade.Sprite('images/paper.png')
+        self.paper.set_position(200,100)
+        self.paper.draw()
+
+        self.scissor = arcade.Sprite('images/scissor.png')
+        self.scissor.set_position(300,100)
+        self.scissor.draw()
+
+        #### RED ####
+        if self.x == 1 :
+            self.rock_red = arcade.Sprite('images/rock red.png')
+            self.rock_red.set_position(200,300)
+            self.rock_red.draw()
+        elif self.x == 2 :
+            self.paper_red = arcade.Sprite('images/paper red.png')
+            self.paper_red.set_position(200,300)
+            self.paper_red.draw()
+        elif self.x == 3 :
+            self.scissor_red = arcade.Sprite('images/scissor red.png')
+            self.scissor_red.set_position(200,300)
+            self.scissor_red.draw()
+        #### BLUE ####
+        elif self.x == 4 :
+            self.rock_blue = arcade.Sprite('images/rock blue.png')
+            self.rock_blue.set_position(200,300)
+            self.rock_blue.draw()
+        elif self.x == 5 :
+            self.paper_blue = arcade.Sprite('images/paper blue.png')
+            self.paper_blue.set_position(200,300)
+            self.paper_blue.draw()
+        elif self.x == 6 :
+            self.scissor_blue = arcade.Sprite('images/scissor blue.png')
+            self.scissor_blue.set_position(200,300)
+            self.scissor_blue.draw()
 
     def on_key_press(self,key,key_modifier):
-        Red_against().on_key_press(key,key_modifier)
+        self.rand.on_key_press(key,key_modifier)
+        self.R.on_key_press(key,key_modifier)
+        self.B.on_key_press(key,key_modifier)
+
 
     def update(self,delta):
-        self.x = rand_rps().x
-        self.y = rand_rps().y
-        ##RED
-        if(self.x==1):
+        #### RED ####
+        self.x = self.rand.update(delta)
+        if self.x == 1 :
             self.red = self.R.Rock()
-            if(self.red):
+            if self.red :
                 self.score+=10
-            else:
-                self.score-=5
-        elif(self.x==2):
+        elif self.x == 2 :
             self.red = self.R.Paper()
-            if(self.red):
+            if self.red :
                 self.score+=10
-            else:
-                self.score-=5
-        elif(self.x==3):
+        elif self.x == 3 :
             self.red = self.R.Scissor()
-            if(self.red):
+            if self.red :
                 self.score+=10
-            else:
-                self.score-=5
 
-        ##BLUE
-        elif(self.x==4):
+        #### BLUE ####
+        elif self.x == 4 :
             self.blue = self.B.Rock()
-            if(self.blue):
+            if self.blue :
                 self.score+=10
-            else:
-                self.score-=5
-        elif(self.x==5):
+        elif self.x == 5 :
             self.blue = self.B.Paper()
-            if(self.blue):
-                self.score+=10
-            else:
-                self.score-=5       
-        elif(self.x==6):
+            if self.blue :
+                self.score += 10
+        elif self.x == 6 :
             self.blue = self.B.Scissor()
-            if(self.blue):
+            if self.blue :
                 self.score+=10
-            else:
-                self.score-=5
+
+        print(self.score)
         ##set score
         if(self.score<0):
             self.score = 0
