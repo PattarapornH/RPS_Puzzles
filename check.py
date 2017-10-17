d2df4cf7c4ff256a83e076340a51bf2b13a95600
PattarapornH <pataraporn.han@gmail.com>
2017-10-17 23:25:31 +0700

    add game over

--- check.py
+++ check.py
@@ -5,20 +5,19 @@ D scissor
 '''
 import arcade
 import random
-#from rand import rand_rps
 
 # Enumeration of RPS
-class RPS:
-    ROCK_RED = 1
-    PAPER_RED = 2
-    SCISSOR_RED = 3
-    ROCK_BLUE = 4
-    PAPER_BLUE = 5
-    SCISSOR_BLUE = 6
+
+ROCK_RED = 1
+PAPER_RED = 2
+SCISSOR_RED = 3
+ROCK_BLUE = 4
+PAPER_BLUE = 5
+SCISSOR_BLUE = 6
 
 class check_win_lost :
     def __init__(self):
-        self.x = random.randint(1, 6)
+        self.x = random.randint(1,6)
         self.is_A_pressed = False
         self.is_S_pressed = False
         self.is_D_pressed = False
@@ -29,58 +28,70 @@ class check_win_lost :
         if key == arcade.key.A:
             self.is_A_pressed = True
             print("A PRESSED")
-            #self.is_A_pressed = self.rand.on_key_press(key,key_modifiers)
         elif key == arcade.key.S:
             self.is_S_pressed = True
             print("S PRESSED")
-            #self.is_S_pressed = self.rand.on_key_press(key,key_modifiers)
         elif key == arcade.key.D:
             print("D PRESSED")
             self.is_D_pressed = True
-            #self.is_D_pressed = self.rand.on_key_press(key,key_modifiers)
         elif key == arcade.key.W:
             print("W PRESSED")
             self.is_W_pressed = True
-            #self.is_W_pressed = self.rand.on_key_press(key,key_modifiers)
         
     def update(self,delta):
         x = self.x
-        is_change_question = self.is_A_pressed or self.is_S_pressed or self.is_D_pressed or self.is_W_pressed
-
-        if x == RPS.ROCK_RED : 
+        self.is_change_question = self.is_A_pressed or self.is_S_pressed or self.is_D_pressed or self.is_W_pressed
+        self.back_to_game_played = self.is_W_pressed
+        #### RED ####
+        if x == ROCK_RED : 
             if self.is_D_pressed:
                 #print("D PRESSED: red rock")
                 self.score += 10
                 self.is_D_pressed = False
-            elif is_change_question:
-                self.score -= 5
-        elif x == RPS.PAPER_RED :
+            elif self.is_change_question:
+                self.score -= 7
+        elif x == PAPER_RED :
             if self.is_A_pressed:
                 #print("A PRESSED: red paper")
                 self.score +=10
                 self.is_A_pressed = False
-        elif x == RPS.SCISSOR_RED :
+            elif self.is_change_question:
+                self.score -= 7
+        elif x == SCISSOR_RED :
             if self.is_S_pressed:
                 #print("S PRESSED: red scissor")
                 self.score +=10
                 self.is_S_pressed = False
-        elif x == RPS.ROCK_BLUE : 
+            elif self.is_change_question:
+                self.score -= 7
+        #### BLUE ####
+        elif x == ROCK_BLUE : 
             if self.is_S_pressed:
                 #print("S PRESSED: blue rock")
                 self.score +=10
                 self.is_S_pressed = False
-        elif x == RPS.PAPER_BLUE :
+            elif self.is_change_question:
+                self.score -= 7
+        elif x == PAPER_BLUE :
             if self.is_D_pressed:
                 #print("D PRESSED: blue paper")
                 self.score +=10
                 self.is_D_pressed = False
-        elif x == RPS.SCISSOR_BLUE :
+            elif self.is_change_question:
+                self.score -= 7
+        elif x == SCISSOR_BLUE :
             if self.is_A_pressed:
                 #print("A PRESSED: blue scissor")
                 self.score +=10
                 self.is_A_pressed = False
+            elif self.is_change_question:
+                self.score -= 7
+        
+        #### SET SCORE OR GAME OVER####
+        if self.score < 0 or self.is_W_pressed:
+            self.score = 0
 
-        if is_change_question:
+        if self.is_change_question:
             print("CHANGE")
             self.x = random.randint(1, 6)
             self.is_A_pressed = False
@@ -89,83 +100,3 @@ class check_win_lost :
             self.is_W_pressed = False
 
         return self.score
-'''
-class Red :
-    def __init__(self,x)
-        self.x = x
-        ### RED ###
-        if self.x == 1 : 
-        #    print("red rock")
-            if self.is_D_pressed:
-                self.score +=10
-                self.is_D_pressed = False
-            else :
-                self.score -= 5
-        elif self.x == 2 :
-            if self.is_A_pressed:
-                self.score +=10
-                self.is_A_pressed = False
-            else :
-                self.score -= 5
-        elif self.x == 3 :
-            if self.is_S_pressed:
-                self.score +=10
-                self.is_S_pressed = False
-            else :
-                self.score -= 5
-        return self.score
-class Blue :
-        ### BLUE ###
-        elif self.x == 4 : 
-            if self.is_S_pressed:
-                self.score +=10
-                self.is_S_pressed = False
-            else :
-                self.score -= 5
-        elif self.x == 5 :
-            if self.is_D_pressed:
-                self.score +=10
-                self.is_D_pressed = False
-            else :
-                self.score -= 5
-        elif self.x == 6 :
-            if self.is_A_pressed:
-                self.score +=10
-                self.is_A_pressed = False
-            else :
-                self.score -= 5
-        
-        return self.score
-#                print("correct")
-#                self.score+=10
-#            else :
-#                self.score-=5
-'''   
-'''        
-    def Rock(self):
-        if self.is_D_pressed :
-            self.sc.score += 10
-            self.is_D_pressed = False
-#            return True
-        else:
-            self.sc.score -= 5
-#            return False
-    
-    def Paper(self):
-        if self.is_A_pressed :
-            self.sc.score += 10
-            self.is_A_pressed = False
-#            return True
-        else:
-            self.sc.score -= 5
-#            return False
-
-    def Scissor(self):
-        if self.is_S_pressed :
-            self.sc.score += 10
-            self.is_S_pressed = False
-#            return True
-        else:
-            self.sc.score -= 5
-#            return False
-'''
