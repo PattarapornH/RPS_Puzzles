283b35c3f59e8aa74a24a1a3605ce2cec21c356d
PattarapornH <pataraporn.han@gmail.com>
2017-10-18 17:06:44 +0700

    full version

--- check.py
+++ check.py
@@ -15,14 +15,19 @@ ROCK_BLUE = 4
 PAPER_BLUE = 5
 SCISSOR_BLUE = 6
 
+GAME_PLAYED = 1
+GAME_OVER = 2
+
 class check_win_lost :
-    def __init__(self):
+    def __init__(self, world):
         self.x = random.randint(1,6)
         self.is_A_pressed = False
         self.is_S_pressed = False
         self.is_D_pressed = False
         self.is_W_pressed = False
+        self.back_to_game_played = None
         self.score = 0
+        self.world = world
 
         #### SOUND ####
         self.sound_correct = arcade.sound.load_sound('sound/correct.mp3')
@@ -44,6 +49,8 @@ class check_win_lost :
         
     def update(self,delta):
         x = self.x
+        if self.world.game_state == GAME_OVER :
+            x = -1
         self.is_change_question = self.is_A_pressed or self.is_S_pressed or self.is_D_pressed
         self.back_to_game_played = self.is_W_pressed
         #### RED ####
@@ -106,13 +113,13 @@ class check_win_lost :
         #### SET SCORE OR GAME OVER####
         if self.score < 0 or self.back_to_game_played:
             self.score = 0
+            self.is_W_pressed = False
 
+        #### RANDOM ####
         if self.is_change_question:
             print("CHANGE")
             self.x = random.randint(1, 6)
             self.is_A_pressed = False
             self.is_D_pressed = False
             self.is_S_pressed = False
-            self.is_W_pressed = False
-        
         return self.score
--- rps.py
+++ rps.py
@@ -12,7 +12,7 @@ class gameWindow(arcade.Window):
     def __init__ (self,width,height):
         super().__init__(width,height,title = "RPS_Puzzles")
         self.background = arcade.load_texture("images/bg1.jpg")
-        self.check = check_win_lost()
+        self.check = check_win_lost(self)
         self.x = self.check.x
         self.score = 0
         self.score_text = None
@@ -20,78 +20,94 @@ class gameWindow(arcade.Window):
         self.time_text = None
         self.game_state = 1
 
-    def on_draw_game(self):
-        arcade.start_render()
-        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
-
-        #### DRAW CHOICE ####
+        #### CHOICE ####
         self.rock = arcade.Sprite('images/rock.png')
         self.rock.set_position(100,100)
-        self.rock.draw()
         self.text_A = arcade.create_text("A", arcade.color.BLACK,15)
-        arcade.render_text(self.text_A,100,50)
 
         self.paper = arcade.Sprite('images/paper.png')
         self.paper.set_position(210,100)
-        self.paper.draw()
         self.text_S = arcade.create_text("S", arcade.color.BLACK,15)
-        arcade.render_text(self.text_S,200,50)
-        
+
         self.scissor = arcade.Sprite('images/scissor.png')
         self.scissor.set_position(300,100)
-        self.scissor.draw()
         self.text_D = arcade.create_text("D", arcade.color.BLACK,15)
-        arcade.render_text(self.text_D,300,50)
 
         #### RED ####
-        if self.x == 1 :
-            self.rock_red = arcade.Sprite('images/rock red.png')
-            self.rock_red.set_position(200,300)   
+        self.rock_red = arcade.Sprite('images/rock red.png')
+        self.rock_red.set_position(200,300)
+        
+        self.paper_red = arcade.Sprite('images/paper red.png')
+        self.paper_red.set_position(200,300)
+
+        self.scissor_red = arcade.Sprite('images/scissor red.png')
+        self.scissor_red.set_position(200,300)          
+
+        #### BLUE ####
+        self.rock_blue = arcade.Sprite('images/rock blue.png')
+        self.rock_blue.set_position(200,300)
+
+        self.paper_blue = arcade.Sprite('images/paper blue.png')
+        self.paper_blue.set_position(200,300)
+
+        self.scissor_blue = arcade.Sprite('images/scissor blue.png')
+        self.scissor_blue.set_position(200,300)
+    def on_draw_game(self):
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+
+        #### DRAW CHOICE ####
+        self.rock.draw()
+        arcade.render_text(self.text_A,100,50)
+
+        self.paper.draw()
+        arcade.render_text(self.text_S,200,50)
+        
+        self.scissor.draw()
+        arcade.render_text(self.text_D,300,50)
+
+        #### DRAW RED ####
+        if self.x == 1 : 
             self.rock_red.draw()
         elif self.x == 2 :
-            self.paper_red = arcade.Sprite('images/paper red.png')
-            self.paper_red.set_position(200,300)
             self.paper_red.draw()
         elif self.x == 3 :
-            self.scissor_red = arcade.Sprite('images/scissor red.png')
-            self.scissor_red.set_position(200,300)
             self.scissor_red.draw()
-        #### BLUE ####
+        #### DRAW BLUE ####
         elif self.x == 4 :
-            self.rock_blue = arcade.Sprite('images/rock blue.png')
-            self.rock_blue.set_position(200,300)
             self.rock_blue.draw()
         elif self.x == 5 :
-            self.paper_blue = arcade.Sprite('images/paper blue.png')
-            self.paper_blue.set_position(200,300)
             self.paper_blue.draw()
         elif self.x == 6 :
-            self.scissor_blue = arcade.Sprite('images/scissor blue.png')
-            self.scissor_blue.set_position(200,300)
             self.scissor_blue.draw()
 
-        #### SCORE ####
+        #### DRAW SCORE ####
         output_score = f"Score : {self.score}"
         if not self.score_text or self.score_text.text != output_score:
             self.score_text = arcade.create_text(output_score, arcade.color.BLACK, 15)
         arcade.render_text(self.score_text,300,475)
-
-        #### TIME ####
+        #### DRAW TIME ####
         output_time = f"Time : {round(self.time)}"
         if not self.time_text or self.time_text.text != output_time :
             self.time_text = arcade.create_text(output_time, arcade.color.BLACK, 15)
         arcade.render_text(self.time_text, 10, 475)
     
     def on_draw_game_over(self):
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
         output_game_over = "GAME OVER"
-        arcade.draw_text(output_game_over, 100, 400, arcade.color.RED_DEVIL, 30)
+        arcade.draw_text(output_game_over, 70, 400, arcade.color.BLACK, 40)
+        output_last_score = f"Score : {round(self.score)}"
+        arcade.draw_text(output_last_score,120,280,arcade.color.BLUE_VIOLET,25)
         output_restart = "Press W to restart"
-        arcade.draw_text(output_restart, 80,175, arcade.color.RED_DEVIL, 25)
+        arcade.draw_text(output_restart, 80,175, arcade.color.BLUE_SAPPHIRE, 25)
+        self.rock.draw()
+        self.paper.draw()
+        self.scissor.draw()
     
     def on_key_press(self,key,key_modifiers):
         self.check.on_key_press(key,key_modifiers)
     
     def on_draw(self):
+        arcade.start_render()
         if self.game_state == GAME_PLAYED :
             self.on_draw_game()
         
